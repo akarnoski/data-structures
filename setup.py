057b1f41ea28840dca36d64ac0d50c493bfbec9b
adriennekarnoski <adrienne.j.karnoski@gmail.com>
2017-10-23 15:43:55 -0700

    setup.py file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="data-structures",
+    description="Data structures.",
+    py_modules=['linked_list'],
+    install_requires=[],
+    extras_require={'test': ['pytest', 'pytest-watch', 'tox']},
+    )
