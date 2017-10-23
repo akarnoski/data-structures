bdb46da80dd1f0a0c74d79183a4c4968f39acb97
Hewlette Dibble <hcodydibble@gmail.com>
2017-10-23 16:26:31 -0700

    LinkedList method of push is working

--- src/linked_list.py
+++ src/linked_list.py
@@ -1,6 +1,19 @@
 """."""
 
 
+class Node(object):
+    """."""
+
+    def __init__(self, val=None, next=None):
+        """."""
+        self.val = val
+        self.next = next
+
+    def __str__(self):
+        """."""
+        return "Node {}".format(self.val)
+
+
 class LinkedList(object):
     """."""
 
