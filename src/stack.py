e7a09195ceab4b8d8b91e585485428fc4cb42124
Hewlette Dibble <hcodydibble@gmail.com>
2017-10-24 17:50:24 -0700

    added doc strings

--- src/stack.py
+++ src/stack.py
@@ -1,4 +1,4 @@
-"""."""
+"""Make a new Stack object that has access to LinkedList methods."""
 from linked_list import LinkedList
 
 
@@ -6,7 +6,7 @@ class Stack(object):
     """Create a Stack object."""
 
     def __init__(self):
-        """."""
+        """Initialize new Stacks with access to LinkedList methods."""
         self.linked_list = LinkedList()
 
     def push(self, val):
@@ -14,9 +14,9 @@ class Stack(object):
         self.linked_list.push(val)
 
     def pop(self):
-        """."""
+        """Pop a thing out of the thing."""
         return self.linked_list.pop()
 
     def __len__(self):
-        """."""
+        """Return the list of the stack."""
         return self.linked_list.size()
