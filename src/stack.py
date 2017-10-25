f53da278bba77b209f101c6150833451756ff660
Hewlette Dibble <hcodydibble@gmail.com>
2017-10-24 17:05:03 -0700

    Stack class maybe kind of working

---
+++ src/stack.py
@@ -0,0 +1,18 @@
+"""."""
+from linked_list import LinkedList
+
+
+class Stack(object):
+    """Create a Stack object."""
+
+    def __init__(self):
+        """."""
+        self.linked_list = LinkedList()
+
+    def push(self, val):
+        """Push a thing onto a thing."""
+        self.linked_list.push(val)
+
+    def pop(self):
+        """."""
+        return self.linked_list.pop()
