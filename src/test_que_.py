ff58b2a5006d9b251cd98b7c67a0dfe2941d48bf
adriennekarnoski <adrienne.j.karnoski@gmail.com>
2017-10-26 16:06:30 -0700

    changes to test

--- src/test_que_.py
+++ src/test_que_.py
@@ -1,8 +1,16 @@
 import pytest
 
 
-def test_queue_is_instance_of_dll_object():
+def test_queue_is_instance_of_queue_object():
     """Test stack is instance of Stack()."""
     from que_ import Queue
     q = Queue()
     assert isinstance(q, Queue)
+
+
+def test_queue_is_instance_of_doubly_linked_list():
+    """Test stack inherits from DoublyLinkedList."""
+    from que_ import Queue
+    from doubly_linked_list import DoublyLinkedList
+    q = Queue()
+    assert isinstance(q.doubly_linked_list, DoublyLinkedList)
