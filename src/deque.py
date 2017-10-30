cffa5c59408be0875b01aefdd8a9f5b3067b03a3
adriennekarnoski <adrienne.j.karnoski@gmail.com>
2017-10-30 13:26:38 -0700

    fixed docstring

--- src/deque.py
+++ src/deque.py
@@ -3,50 +3,50 @@ from doubly_linked_list import DoublyLinkedList
 
 
 class Deque(object):
-    """Create a Queue object."""
+    """Create a Deque object."""
 
     def __init__(self):
-        """Initialize Queue using DoublyLinkedList methods."""
+        """Initialize Deque using DoublyLinkedList methods."""
         self._doubly_linked_list = DoublyLinkedList()
 
     def append(self, val):
-        """Add a node to queue."""
+        """Add a node to back Deque."""
         self._doubly_linked_list.append(val)
 
     def appendleft(self, val):
-        """Add a node to queue."""
+        """Add a node to front Deque."""
         self._doubly_linked_list.push(val)
 
     def pop(self):
-        """Remove first node pushed in."""
+        """Remove node at the back."""
         try:
             return self._doubly_linked_list.shift()
         except IndexError:
             raise IndexError('Nothing to pop')
 
     def popleft(self):
-        """Remove first node pushed in."""
+        """Remove node at the front."""
         try:
             return self._doubly_linked_list.pop()
         except IndexError:
             raise IndexError('Nothing to pop from front')
 
     def peek(self):
-        """Get value of next node to be dequeued."""
+        """Get value of next node to be popped left."""
         if self._doubly_linked_list.size() == 0:
             return None
         return self._doubly_linked_list.tail.data
 
-    def peek(self):
-        """Get value of next node to be dequeued."""
+    def peekleft(self):
+        """Get value of next node to be popped."""
         if self._doubly_linked_list.size() == 0:
             return None
         return self._doubly_linked_list.head.data
 
     def size(self):
-        """Get the size of the queue."""
+        """Get the size of the Deque."""
         return self._doubly_linked_list.size()
 
     def __len__(self):
-        """Return the length of the queue using Python len function."""
+        """Return the length of the Deque using Python len function."""
         return self.size()
