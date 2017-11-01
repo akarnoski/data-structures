352c33671aa00ed29bf3445f2e7ca5bcff053a42
adriennekarnoski <adrienne.j.karnoski@gmail.com>
2017-11-01 15:12:08 -0700

    more tests

--- src/priorityq.py
+++ src/priorityq.py
@@ -14,10 +14,17 @@ class PriorityQueue(object):
 
     def pop(self):
         """Pop first highest priority value."""
-        highest_priority = max(self._priority_dict, key=int)
-        return self._priority_dict[highest_priority].pop(0)
+        if self._priority_dict == {}:
+            raise IndexError('Nothing to pop')
+        try:
+            highest_priority = max(self._priority_dict, key=int)
+            return self._priority_dict[highest_priority].pop(0)
+        except IndexError:
+            raise IndexError('Nothing to pop')
 
     def peek(self):
         """Show the first highest priority value."""
+        if self._priority_dict == {}:
+            return None
         highest_priority = max(self._priority_dict, key=int)
         return self._priority_dict[highest_priority][0]
--- src/test_priorityq.py
+++ src/test_priorityq.py
@@ -1,8 +1,10 @@
 """Test functions for priorityq module."""
 
+import pytest
 
-def test_pop_works_correctly():
-    """."""
+
+def test_pop_removes_highest_priority():
+    """Test pop method removed first value in highest priority."""
     from priorityq import PriorityQueue
     p = PriorityQueue()
     for i in range(3):
@@ -12,8 +14,26 @@ def test_pop_works_correctly():
     assert p.pop() == 8
 
 
-def test_peek_works_correctly():
-    """."""
+def test_pop_on_new_instance_raises_error():
+    """Test pop method on new priority queue raises error."""
+    from priorityq import PriorityQueue
+    p = PriorityQueue()
+    with pytest.raises(IndexError):
+        p.pop()
+
+
+def test_pop_on_emptied_queue_raises_error():
+    """Test pop method on empties priority queue raises error."""
+    from priorityq import PriorityQueue
+    p = PriorityQueue()
+    p.insert(2)
+    p.pop()
+    with pytest.raises(IndexError):
+        p.pop()
+
+
+def test_peek_shows_highest_priority():
+    """Test the peek method to show highest priority value."""
     from priorityq import PriorityQueue
     p = PriorityQueue()
     for i in range(3):
@@ -22,3 +42,10 @@ def test_peek_works_correctly():
     p.insert(10, 2)
     p.insert(0, 33)
     assert p.peek() == 0
+
+
+def test_peek_on_empty_priority_queue_returns_none():
+    """Test the peek method returns none if no values available."""
+    from priorityq import PriorityQueue
+    p = PriorityQueue()
+    assert p.peek() is None
