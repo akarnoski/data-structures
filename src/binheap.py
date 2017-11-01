750c990e0159c7bce3a4576c88ad1076f4842e61
adriennekarnoski <adrienne.j.karnoski@gmail.com>
2017-11-01 13:16:16 -0700

    FINALLY WORKING

--- src/binheap.py
+++ src/binheap.py
@@ -1,19 +1,29 @@
-"""Build a binary heap object."""
+"""Build a binary min heap object."""
+from math import floor
 
 
 class BinaryHeap(object):
-    """Create a Binary Heap object."""
+    """Create a Binary Heap object as a Min Heap."""
 
     def __init__(self):
-        """Initialize."""
+        """Initialize the heap list to be used by Binary Heap."""
         self.heap_list = []
 
     def push(self, val):
-        """Add new value."""
+        """Add new value to heap list and run check heap method."""
         self.heap_list.append(val)
+        if len(self.heap_list) == 2:
+            self._small_heap()
+        self._check_heap()
+        return heap
+
+    def _small_heap(self):
+        heap = self.heap_list
+        if heap[0] > heap[1]:
+            heap[0], heap[1] = heap[1], heap[0]
+        return heap
 
     def _check_heap(self):
-        from math import floor
         """Check all the children are less than their parents."""
         heap = self.heap_list
         index = floor((len(heap) - 1) / 2)
@@ -21,7 +31,6 @@ class BinaryHeap(object):
         while i < index:
             l = (2 * i) + 1
             if heap[i] > heap[l]:
-                print(i)
                 heap[i], heap[l] = heap[l], heap[i]
             try:
                 r = (2 * i) + 2
@@ -31,6 +40,22 @@ class BinaryHeap(object):
                 pass
             i += 1
         return heap
+
+    def pop(self):
+        """Remove top value of heap and run check heap method."""
+        try:
+            heap = self.heap_list
+            index = len(heap) - 1
+            heap[0], heap[index] = heap[index], heap[0]
+            print(heap[0])
+            print(heap[index])
+            self.heap_list.pop()
+            if len(self.heap_list) == 2:
+                self._small_heap()
+            self._check_heap()
+            return heap
+        except IndexError:
+            raise IndexError('Nothing available to pop')
 
     def _display(self):
         """Make it easier during testing."""
