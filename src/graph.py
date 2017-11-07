e6923595b50e78fcd5d1e0a4994e7f50fe5dedd7
Hewlette Dibble <hcodydibble@gmail.com>
2017-11-07 14:58:57 -0800

    re-did entire Graph

--- src/graph.py
+++ src/graph.py
@@ -1,106 +1,66 @@
 """Create a Graph class data structure."""
 
 
-class Node(object):
-    """Node object to put into the Graph."""
-
-    def __init__(self, value=None):
-        """Node things on initialization."""
-        self.value = value
-
-    def __repr__(self):
-        """Make Node objects do repr things."""
-        return str(self.value)
-
-
 class Graph(object):
     """Graph object."""
 
     def __init__(self):
         """Graph things on initialization."""
-        self._nodes_list = []
-        self._check_list = []
-        self._edge_list = {}
+        self._nodes = {}
 
     def nodes(self):
         """List all nodes in the Graph."""
-        return self._nodes_list
+        return [key for key in self._nodes]
 
     def edges(self):
         """List all edges in the Graph."""
-        return self._edge_list
+        return self._nodes
 
-    def add_node(self, val):
+    def add_node(self, *args):
         """Add a new node to the Graph."""
-        if val in self._check_list:
-            raise ValueError("Value already in Graph.")
-        new_node = Node(val)
-        self._nodes_list.append(new_node)
-        self._check_list.append(val)
-        return new_node
+        for arg in args:
+            self._nodes.setdefault(arg, [])
 
     def add_edge(self, val1, val2):
         """Add a new edge to the Graph and connects the values.
 
         If either value doesn't exist they will be created.
         """
-        try:
-            node1 = self.add_node(val1)
-        except ValueError:
-            for node in self._nodes_list:
-                if val1 == node.value:
-                    node1 = node
-
-        try:
-            node2 = self.add_node(val2)
-        except ValueError:
-            for node in self._nodes_list:
-                if val2 == node.value:
-                    node2 = node
-        try:
-            edge_list = self._edge_list[val1]
-            for node in edge_list:
-                if val2 == node.value:
-                    edge_list.remove(node)
-        except KeyError:
+        self.add_node(val1, val2)
+        if val2 in self._nodes[val1]:  # pragma: no cover
             pass
-        self._edge_list.setdefault(node1.value, []).append(node2)
+        else:
+            self._nodes[val1].append(val2)
 
     def del_node(self, val):
         """Delete the node containing the given value."""
-        if self._nodes_list == []:
-            raise IndexError("No Nodes in Graph.")
-        for node in self._nodes_list:
-            if node.value == val:
-                return self._nodes_list.remove(node)
-        raise IndexError("No such Node in Graph.")
+        try:
+            del self._nodes[val]
+            for key in self._nodes:
+                if val in self._nodes[key]:
+                    self._nodes[key].remove(val)
+        except KeyError:
+            raise KeyError("No such Node exists")
 
     def del_edge(self, val1, val2):
         """Delete the edge connecting the two values."""
         try:
-            edge_list = self._edge_list[val1]
-            for node in edge_list:
-                if val2 == node.value:
-                    return edge_list.remove(node)
+            for node in self._nodes[val1]:
+                if val2 == node:
+                    self._nodes[val1].remove(node)
+                    return
             raise IndexError("No connection between those two Nodes.")
         except KeyError:
             raise KeyError("No Node with given first value.")
 
     def has_node(self, val):
         """Return True is node containing value exists in Graph."""
-        for node in self._nodes_list:
-            if node.value == val:
-                return True
-        return False
+        return True if val in self._nodes else False
 
     def neighbors(self, val):
         """Return list of all nodes connected to the node containing the value."""
-        return self._edge_list[val]
+        return self._nodes[val]
 
     def adjacent(self, val1, val2):
         """Return True if there is an edge connecting the two values."""
-        edge_list = self._edge_list[val1]
-        for node in edge_list:
-            if val2 == node.value:
-                return True
-        return False
+        return val2 in self._nodes[val1]
--- src/test_graph.py
+++ src/test_graph.py
@@ -2,32 +2,25 @@
 import pytest
 
 
-def test_node_object_inits_with_no_val():
-    """Test that Node object initializes without a value."""
-    from graph import Node
-    n = Node()
-    assert n.value is None
-
-
-def test_graph_inits_with_empty_nodes_list(graph_fixture):
-    """Test that Graph initializes with empty nodes list."""
-    assert graph_fixture._nodes_list == []
+def test_graph_inits_with_empty_nodes_dict(graph_fixture):
+    """Test that Graph initializes with empty nodes dict."""
+    assert graph_fixture._nodes == {}
 
 
 def test_add_node_adds_to_nodes_list(graph_fixture):
     """Test that adding a node attaches it to the Graph's node list."""
     graph_fixture.add_node(1)
-    assert len(graph_fixture._nodes_list) == 1
-    assert graph_fixture._nodes_list[0].value == 1
+    assert len(graph_fixture._nodes) == 1
+    assert graph_fixture._nodes[1] == []
 
 
 def test_del_node_removes_node_with_given_value(graph_fixture):
     """Test that removing the node with the given value works."""
     for i in range(5):
         graph_fixture.add_node(i)
-    assert len(graph_fixture._nodes_list) == 5
+    assert len(graph_fixture._nodes) == 5
     graph_fixture.del_node(3)
-    assert len(graph_fixture._nodes_list) == 4
+    assert len(graph_fixture._nodes) == 4
 
 
 def test_has_node_returns_true_if_node_in_graph(graph_fixture):
@@ -44,32 +37,21 @@ def test_has_node_returns_false_if_node_not_in_graph(graph_fixture):
     assert graph_fixture.has_node(7) is False
 
 
-def test_add_node_raises_value_error(graph_fixture):
-    """Test add_node raises ValueError.
-
-    Will raise the error if attempting to add a Node with a value that
-    is already in the Graph.
-    """
-    graph_fixture.add_node(2)
-    with pytest.raises(ValueError):
-        graph_fixture.add_node(2)
-
-
-def test_del_node_raises_index_error_on_empty_graph(graph_fixture):
-        """Test del_node raises IndexError if no Nodes in Graph."""
-        with pytest.raises(IndexError):
+def test_del_node_raises_key_error_on_empty_graph(graph_fixture):
+        """Test del_node raises KeyError if no Nodes in Graph."""
+        with pytest.raises(KeyError):
             graph_fixture.del_node(2)
 
 
-def test_del_node_raises_index_error_on_a_list(graph_fixture):
-    """Test del_node raises IndexError.
+def test_del_node_raises_key_error_on_a_list(graph_fixture):
+    """Test del_node raises KeyError.
 
     Will raise the error if looking for a Node with the given value and it
     is not in the Graph.
     """
     for i in range(3):
         graph_fixture.add_node(i)
-    with pytest.raises(IndexError):
+    with pytest.raises(KeyError):
         graph_fixture.del_node(8)
 
 
@@ -85,14 +67,6 @@ def test_adjacent_returns_true(graph_fixture):
     assert graph_fixture.adjacent(1, 2) is True
 
 
-def test_del_edge_removes_connection(graph_fixture):
-    """Test del_edge removes the connection between two Nodes."""
-    graph_fixture.add_edge(1, 2)
-    graph_fixture.add_edge(1, 3)
-    graph_fixture.del_edge(1, 3)
-    assert len(graph_fixture.edges()[1]) == 1
-
-
 def test_del_edge_raises_error(graph_fixture):
     """Test del_edge raises IndexError.
 
@@ -102,14 +76,6 @@ def test_del_edge_raises_error(graph_fixture):
     graph_fixture.add_edge(1, 2)
     with pytest.raises(IndexError):
         graph_fixture.del_edge(1, 3)
-
-
-def test_add_edge_overwrites_edge(graph_fixture):
-    """Test that an edge gets overwritten if it is created again."""
-    graph_fixture.add_edge(1, 2)
-    assert len(graph_fixture.edges()[1]) == 1
-    graph_fixture.add_edge(1, 2)
-    assert len(graph_fixture.edges()[1]) == 1
 
 
 def test_neighbors_returns_all_connected_nodes(graph_fixture):
@@ -125,3 +91,37 @@ def test_nodes_returns_list_of_nodes(graph_fixture):
     for i in range(4):
         graph_fixture.add_node(i)
     assert len(graph_fixture.nodes()) == 4
+
+
+def test_edges_returns_all_edges(graph_fixture):
+    """Test that edges will return a dict of all edges."""
+    graph_fixture.add_edge(1, 2)
+    graph_fixture.add_edge(2, 3)
+    graph_fixture.add_edge(3, 7)
+    assert graph_fixture.edges() == {1: [2], 2: [3], 3: [7], 7: []}
+
+
+def test_del_node_removes_all_memory_of_node(graph_fixture):
+    """Test that the deletion of the Node goes all the way down the family."""
+    graph_fixture.add_edge(1, 2)
+    graph_fixture.add_edge(2, 1)
+    graph_fixture.del_node(1)
+    assert graph_fixture.nodes() == [2]
+    assert graph_fixture.edges() == {2: []}
+
+
+def test_del_edge_works(graph_fixture):
+    """Test that del_edge will actually delete the edge."""
+    graph_fixture.add_edge(1, 2)
+    graph_fixture.add_edge(1, 3)
+    graph_fixture.del_edge(1, 2)
+    assert graph_fixture.edges() == {1: [3], 2: [], 3: []}
+
+
+def test_del_edge_raises_key_error(graph_fixture):
+    """Test that del_edge raises KeyError.
+
+    Error will be raised if the first given value is not in the Graph.
+    """
+    with pytest.raises(KeyError):
+        graph_fixture.del_edge(1, 2)
