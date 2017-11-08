cc481afe894d8e875f7c0874655ee0764af483a8
Hewlette Dibble <hcodydibble@gmail.com>
2017-11-07 17:04:46 -0800

    ...

--- src/linked_list.py
+++ src/linked_list.py
@@ -54,25 +54,29 @@ class LinkedList(object):
                 search_through = search_through.next
         return search_through
 
-    def remove(self, val):
+    def remove(self, node):
         """Remove selected node."""
         current_node = self.head
         previous_node = None
         found = False
         if current_node is None:
-            return ValueError("No such item.")
-        while current_node or found is False:
-            if val == current_node.data:
-                found = True
+            raise IndexError("Nothing in the list.")
+        try:
+            while current_node and found is False:
+                if node == current_node.data:
+                    found = True
+                else:
+                    previous_node = current_node
+                    current_node = current_node.next
+            if previous_node is None:
+                self.pop()
+            elif current_node.next is None:
+                previous_node.next = None
             else:
-                previous_node = current_node
-                current_node = current_node.next
-        if previous_node is None:
-            self.pop()
-        elif current_node.next is None:
-            previous_node.next = None
-        else:
-            previous_node.next = current_node.next
+                previous_node.next = current_node.next
+        except AttributeError:
+            raise ValueError("No such node.")
+        self._counter -= 1
 
     def display(self):
         """Display nodes in linked list."""
