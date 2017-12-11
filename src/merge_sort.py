dc5106f1e6d4614c948615ab2c46cc6cbc024b62
adriennekarnoski <adrienne.j.karnoski@gmail.com>
2017-12-11 10:34:20 -0800

    added my times

--- src/merge_sort.py
+++ src/merge_sort.py
@@ -30,3 +30,13 @@ def _sort(one_half, two_half):
     else:
         output += one_half
     return output
+
+
+if __name__ == '__main__':  # pragma: no cover
+    import timeit
+    good = [1, 2, 3, 4, 5, 6]
+    bad = [6, 5, 4, 3, 2, 1]
+    average = [2, 1, 5, 4, 6]
+    print('A good time: {}'.format(timeit.timeit("merge_sort(good)", setup="from __main__ import merge_sort, good")))
+    print('A bad time: {}'.format(timeit.timeit("merge_sort(bad)", setup="from __main__ import merge_sort, bad")))
+    print('An average time: {}'.format(timeit.timeit("merge_sort(average)", setup="from __main__ import merge_sort, average")))
