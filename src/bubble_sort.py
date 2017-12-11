ee4c949615325ef949d15bd1507e3e9af1e32946
adriennekarnoski <adrienne.j.karnoski@gmail.com>
2017-12-11 10:36:52 -0800

    added my time stuff

--- src/bubble_sort.py
+++ src/bubble_sort.py
@@ -15,4 +15,11 @@ def bubble_sort(input):
     return input
 
 
-# if __name__ == '__main__':
+if __name__ == '__main__':  # pragma: no cover
+    import timeit
+    good = [1, 2, 3, 4, 5, 6]
+    bad = [6, 5, 4, 3, 2, 1]
+    average = [2, 1, 5, 4, 6]
+    print('A good time: {}'.format(timeit.timeit("bubble_sort(good)", setup="from __main__ import bubble_sort, good")))
+    print('A bad time: {}'.format(timeit.timeit("bubble_sort(bad)", setup="from __main__ import bubble_sort, bad")))
+    print('An average time: {}'.format(timeit.timeit("bubble_sort(average)", setup="from __main__ import bubble_sort, average")))
