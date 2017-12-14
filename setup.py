10738437960bca2092a877afa270c85133eca248
adriennekarnoski <adrienne.j.karnoski@gmail.com>
2017-12-14 15:16:07 -0800

    moving stuff around

--- setup.py
+++ setup.py
@@ -17,5 +17,5 @@ setup(
         ],
     install_requires=[],
     extras_require={'test': ['pytest', 'pytest-watch', 'tox']},
-    package_dir={"": "src"}
+    package_dir={"": "data-structures"}
 )
